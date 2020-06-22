3825d97673a45a044c5bc2d42ffc3b690c3c8043
shortland <ilankleiman@gmail.com>
2020-06-22 14:40:29 -0400

    maint: cleaup & nits

--- src/api.go
+++ src/api.go
@@ -13,30 +13,48 @@ import (
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", handleRoot).Methods("GET")
-	router.HandleFunc("/data/profile/{auth}", handleProfile).Methods("GET")
+	router.HandleFunc("/", handleRoot).Methods(http.MethodGet, http.MethodOptions)
+	router.HandleFunc("/data/profile/{auth}", handleProfile).Methods(http.MethodGet, http.MethodOptions)
+	router.Use(mux.CORSMethodMiddleware(router))
 
 	log.Print("running")
-	account.Load("test")
 	log.Fatal(http.ListenAndServe(":2468", router))
 }
 
+func writeStandardHeaders(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+}
+
 func handleRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "root api page")
+	writeStandardHeaders(w)
+
+	response := map[string]string{
+		"response": "root api page",
+	}
+
+	json, err := json.Marshal(response)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	fmt.Fprint(w, string(json))
 }
 
 func handleProfile(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+	writeStandardHeaders(w)
 
 	acc, err := account.Load(mux.Vars(r)["auth"])
 	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(w, "error loading account")
 		return
 	}
 
 	accountData, err := json.Marshal(acc)
 	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(w, "unable to marshal account json")
 		return
 	}
