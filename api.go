5c6c60142b0a063cf8e13825721899ca756438b3
shortland <ilankleiman@gmail.com>
2020-06-21 23:31:43 -0400

    feat: add wip temp proj

---
+++ api.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+
+	"github.com/gorilla/mux"
+
+	"tower/src/account"
+)
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", handleRoot).Methods("GET")
+	router.HandleFunc("/auth/login", handleLogin).Methods("POST")
+	router.HandleFunc("/auth/register", handleRegister).Methods("POST")
+	router.HandleFunc("/data/profile/{auth}", handleProfile).Methods("GET")
+
+	log.Print("running")
+	log.Fatal(http.ListenAndServe(":2468", router))
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "root api page")
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	email := mux.Vars(r)["email"]
+	password := mux.Vars(r)["password"]
+
+	fmt.Fprintf(w, "Login\nemail: %s, password: %s", email, password)
+}
+
+func handleRegister(w http.ResponseWriter, r *http.Request) {
+	email := mux.Vars(r)["email"]
+	password := mux.Vars(r)["password"]
+
+	fmt.Fprintf(w, "Register\nemail: %s, password: %s", email, password)
+}
+
+func handleProfile(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	
+	acc, err := account.Load(mux.Vars(r)["auth"])
+	if err != nil {
+		fmt.Fprint(w, "error loading account")
+		return
+	}
+	
+	accountData, err := json.Marshal(acc)
+	if err != nil {
+		fmt.Fprint(w, "unable to marshal account json")
+		return
+	}
+
+	fmt.Fprintf(w, "%s", string(accountData))
+	// fmt.Fprintf(w, "%s", fileContents)
+}
---
+++ src/account/account.go
@@ -0,0 +1,71 @@
+package account
+
+import (
+	"os"
+	"fmt"
+	"log"
+	"io/ioutil"
+	"encoding/json"
+)
+
+type Account struct {
+	user	User	`json:"user"`
+}
+
+type User struct {
+    username	string	`json:"username"`
+    password	string	`json:"password"`
+    hash		string	`json:"hash"`
+	email		string	`json:"email"`
+	verified	bool	`json:"verified"`
+	deviceId	string	`json:"device-identifier"`
+	tower		Tower	`json:"tower"`
+}
+
+type Tower struct {
+	level	int		`json:"level"`
+	exp		int		`json:"exp"`
+	cash	int		`json:"cash"`
+	gold	int		`json:"gold"`
+	name	string	`json:"tower-name"`
+	floors	[]Floor	`json:"floors"`
+}
+
+type Floor struct {
+	name			string	`json:"name"`
+	industry		string	`json:"industry-type"`
+	level			int		`json:"level"`
+	currentWorkers	int		`json:"current-workers"`
+	maxWorkers		int		`json:"max-workers"`
+	monthlyRent		int		`json:"monthly-rent"`
+	construction	bool	`json:"under-construction"`
+}
+
+func Load(accountId string) (*Account, error) {
+	var a Account
+
+	jsonFile, err := os.Open("static/user.json")
+	if err != nil {
+		fmt.Println(err)
+		return &a, err
+	}
+
+	defer jsonFile.Close()
+
+	// TODO: Temporarily loads a static user file
+	jsonBytes, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		log.Print(err)
+		return &a, err
+	}
+
+	if err := json.Unmarshal(jsonBytes, &a); err != nil {
+		log.Print(err)
+		return &a, err
+    }
+	
+	fmt.Printf("The user is: %+v\n", a)
+	// fmt.Printf("The tower level is: %d\n", a.user.tower.level)
+
+	return &a, nil
+}
